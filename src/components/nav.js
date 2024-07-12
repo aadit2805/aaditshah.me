94b63bc964726ec9f244279c02d394c9c3cbff79
Aadit Shah <aadit2805@gmail.com>
2024-07-12 18:17:27 -0500

    fixed nav bar

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,23 +1,22 @@
-
 import Link from 'next/link';
 
 const Nav = () => {
   return (
     <nav className="bg-black border-b border-gray-200 px-4 py-2 flex justify-between items-center">
       <div className="text-xl font-semibold text-white">
-        <Link href="/" legacyBehavior>
-          <a className="text-white hover:text-gray-300">aadit shah</a>
+        <Link href="/" className="text-white hover:text-gray-300">
+          aadit shah
         </Link>
       </div>
       <div className="space-x-8">
         <a href="/static/resume.pdf" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-300">
           resume
         </a>
-        <Link href="/about" legacyBehavior>
-          <a className="text-white hover:text-gray-300">about me</a>
+        <Link href="/portfolio" className="text-white hover:text-gray-300">
+          portfolio
         </Link>
-        <Link href="/portfolio" legacyBehavior>
-          <a className="text-white hover:text-gray-300">portfolio</a>
+        <Link href="/reviews" className="text-white hover:text-gray-300">
+          reviews
         </Link>
       </div>
     </nav>
