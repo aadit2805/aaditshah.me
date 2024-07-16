3b65a6347888fd1341d82d1e4eb65e5f626b43b9
Aadit Shah <aadit2805@gmail.com>
2024-07-15 19:10:05 -0500

    ui changes

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import '../app/globals.css'; // Ensure this is the correct path
+import '../app/globals.css'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSun, faMoon } from '@fortawesome/free-solid-svg-icons';
 import { library } from '@fortawesome/fontawesome-svg-core';
@@ -9,22 +9,26 @@ library.add(faSun, faMoon);
 
 const Navbar = ({ isDarkMode, toggleTheme }) => {
   return (
-    <nav className={`flex items-center justify-between p-4 ${isDarkMode ? 'bg-black' : 'bg-white'} shadow-md`}>
+    <nav className="fixed top-0 left-0 right-0 z-50 flex items-center justify-between p-4 shadow-md bg-opacity-100">
       <Link href="/" legacyBehavior>
-        <a className="ml-4 text-lg font-semibold nav-link">{isDarkMode ? 'aadit shah' : 'aadit shah'}</a>
+        <a className="ml-4 text-lg font-semibold nav-link gradient large">aadit shah</a>
       </Link>
       <div className="flex items-center space-x-3">
-        <Link href="/portfolio" legacyBehavior>
-          <a className="nav-link text-lg font-semibold">{isDarkMode ? 'portfolio' : 'portfolio'}</a>
+        <Link href="/music" legacyBehavior>
+          <a className="nav-link text-lg font-semibold">music</a>
         </Link>
         <Link href="/reviews" legacyBehavior>
-          <a className="nav-link text-lg font-semibold">{isDarkMode ? 'reviews' : 'reviews'}</a>
+          <a className="nav-link text-lg font-semibold">reviews</a>
         </Link>
         <Link href="/resume.pdf" legacyBehavior>
-          <a className="nav-link text-lg font-semibold" target="_blank" rel="noopener noreferrer">{isDarkMode ? 'resume' : 'resume'}</a>
+          <a className="nav-link text-lg font-semibold" target="_blank" rel="noopener noreferrer">resume</a>
         </Link>
-        <button onClick={toggleTheme} className="nav-button text-lg font-semibold">
-          {isDarkMode ? <FontAwesomeIcon icon={faSun} /> : <FontAwesomeIcon icon={faMoon} />}
+        <button 
+          onClick={toggleTheme} 
+          className="nav-button text-lg font-semibold" 
+          aria-label={isDarkMode ? 'Change to light theme' : 'Change to dark theme'}
+        >
+          <FontAwesomeIcon icon={isDarkMode ? faSun : faMoon} />
         </button>
       </div>
     </nav>
--- src/lib/fontAwesome.js
+++ src/lib/fontAwesome.js
@@ -1,5 +1,5 @@
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faSun, faMoon, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
-library.add(faEnvelope, faGithub, faLinkedin, faTwitter);
+library.add(faSun, faMoon, faEnvelope, faGithub, faLinkedin, faTwitter);
